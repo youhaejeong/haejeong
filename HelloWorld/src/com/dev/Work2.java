8b19b6265cf653e94926e56a6d1149cc337488c2
DANAWA <DANAWA@192.168.56.1>
2022-03-16 09:05:33 +0900

    03.15 과제 최최종

--- HelloWorld/src/com/dev/Work2.java
+++ HelloWorld/src/com/dev/Work2.java
@@ -26,7 +26,7 @@ public class Work2 {
 			System.out.println(num1 + ">" + num2 + ">" + num3);
 		} else if (num3 < num1 && num1 < num2) {
 			System.out.println(num2 + ">" + num1 + ">" + num3);
-		}
+		}scn.close();
 	}
 
 }
