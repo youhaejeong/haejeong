06c7737a7fee17fe308dffcdf12ef7adeece203b
admin <admin@YD-04>
2022-03-23 17:42:41 +0900

    2022.03.23 17:42 찐찐쉅 마지막 커밋

--- p20220321/src/com/edu/collect/HashTableExample.java
+++ p20220321/src/com/edu/collect/HashTableExample.java
@@ -1,39 +1,38 @@
 package com.edu.collect;
 
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*; //자바 언틸 밑에 있는것들을 모두 임포트
 
 public class HashTableExample {
 	public static void main(String[] args) {
 		Map<String, String> map = new Hashtable<String, String>();
-		
-		map.put("string","12");
-		map.put("summer","123");
-		map.put("fall","1234");
-		map.put("winter","12345");
-		
+
+		map.put("string", "12");
+		map.put("summer", "123");
+		map.put("fall", "1234");
+		map.put("winter", "12345");
+
 		Scanner scn = new Scanner(System.in);
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("아이디와 비밀번호를 입력해주세요");
 			System.out.println("아이디: ");
-			String id =scn.nextLine();
+			String id = scn.nextLine();
 			System.out.println("비밀번호: ");
 			String password = scn.nextLine();
 			System.out.println();
-			
-			if(map.containsKey(id)) {
-				if (map.get(id).equals(password)){
+
+			if (map.containsKey(id)) {
+				if (map.get(id).equals(password)) {
 					System.out.println("로그인되었습니다.");
 					break;
-				}else {
+				} else {
 					System.out.println("비밀번호가 일치하지 않습니다.");
 				}
-			}else {
+			} else {
 				System.out.println("입력하신 아이디가 존재하지 않습니다.");
 			}
 		}
+		scn.close();
 	}
 
 }
